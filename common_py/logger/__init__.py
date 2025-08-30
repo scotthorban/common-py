d69962c6c8967d0851bdea1b3c2237e583286684
Scott Horban <161889944+scotthorban@users.noreply.github.com>
2025-08-30 16:38:50 +0100

    2 add json logger to common py (#7)
    * feat: #2 add common logger source code
    
    * feat: #2 add package build info to pyproject.toml
    
    * docs: #2 update pyproject.toml and CHANGELOG.md
    
    * chore: #2 update uv.lock
    
    * fix: #2 use neater formatter default assignment code

---
+++ common_py/logger/__init__.py
@@ -0,0 +1,4 @@
+from common_py.logger.formatters import JSON_FORMATTER
+from common_py.logger.logger import get_json_logger, get_logger
+
+__all__ = ["JSON_FORMATTER", "get_json_logger", "get_logger"]
