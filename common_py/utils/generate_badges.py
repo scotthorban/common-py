0c3c1ae283ae568dec2583712f528738e6a391d0
scotthorban <scotthorban@gmail.com>
2025-11-06 21:16:36 +0000

    feat: #4 add coverage badge generation logic

--- common_py/utils/generate_badges.py
+++ common_py/utils/generate_badges.py
@@ -17,58 +17,74 @@ class BadgeGenerator:
 
     Attributes:
         output_path (Path): Path to the output directory.
+        python_version (str): Python version.
+        tests_report_path (Path): Path to the unit tests XML report.
+        coverage_report_path (Path): Path to the unit test coverage XML report.
     """
 
-    def __init__(self, output_dir: str | Path, python_version: str | None, tests_report_path: str | Path) -> None:
+    def __init__(
+        self,
+        output_dir: str | Path,
+        python_version: str | None,
+        tests_report_path: str | Path,
+        coverage_report_path: str | Path,
+    ) -> None:
         """Initializes a BadgeGenerator instance."""
-        self.output_path = output_dir if isinstance(output_dir, Path) else Path(output_dir)
+        self.output_path = Path(output_dir)
         self.python_version = python_version
-        self.tests_report_path = tests_report_path if isinstance(tests_report_path, Path) else Path(tests_report_path)
+        self.tests_report_path = Path(tests_report_path)
+        self.coverage_report_path = Path(coverage_report_path)
 
     def generate_badges(self) -> None:
         """Generate project badges based on arguments provided to the class."""
         if self.python_version:
-            self.make_badge(label="python", value=self.python_version, filename="python.svg", color=BLUE)
+            self.make_badge(label="python", value=self.python_version, filename="python.svg", colour=BLUE)
+
         if self.tests_report_path:
             results = self.get_unittest_results()
-            self.make_badge(label="unittest", value=results[0], filename="unittest.svg", color=results[1])
+            self.make_badge(label="unittest", value=results[0], filename="unittest.svg", colour=results[1])
 
-    def make_badge(self, label: str, value: str, filename: str, color: str) -> None:
-        """Creates a badge using the given label, value and color, saving the result to filename.
+        if self.coverage_report_path:
+            results = self.get_coverage_results()
+            self.make_badge(label="coverage", value=results, filename="coverage.svg")
+
+    def make_badge(self, label: str, value: str, filename: str, colour: str | None = None) -> None:
+        """Creates a badge using the given label, value and colour, saving the result to filename.
 
         Existing badges will be removed before the new badge is generated.
         Parameters:
             label (str): The label of the badge.
             value (str): The value of the badge.
             filename (str): The filename where the badge will be saved.
-            color (str): The color of the badge.
+            colour (str | None): Optional colour of the badge.
+                Anybadge will infer the colour if omitted and the value is an integer between 0 and 100.
         """
         img_path = self.output_path.joinpath(filename)
         img_path.unlink(missing_ok=True)
 
-        subprocess.run(
-            args=[
-                "uv",
-                "run",
-                "anybadge",
-                "-l",
-                label,
-                "-v",
-                str(value),
-                "-f",
-                str(img_path),
-                "--color",
-                color,
-            ],
-            check=False,
-        )
+        args = [
+            "uv",
+            "run",
+            "anybadge",
+            "-l",
+            label,
+            "-v",
+            str(value),
+            "-f",
+            str(img_path),
+        ]
+
+        if colour:
+            args.extend(["--color", colour])
+
+        subprocess.run(args=args, check=False)
 
     def get_unittest_results(self) -> tuple[str, str]:
         """Read and return test results from a unit-tests.xml file.
 
-        Returns a tuple of the test results and the color of the badge.
+        Returns a tuple of the test results and the colour of the badge.
         """
-        root = ElementTree.parse(self.tests_report_path).getroot()
+        root = ElementTree.parse(source=self.tests_report_path).getroot()
 
         failures = 0
         skipped = 0
@@ -86,6 +102,15 @@ class BadgeGenerator:
 
         return result_mapping.get(True)
 
+    def get_coverage_results(self) -> str:
+        """Read and return coverage from a unit-tests.xml file."""
+        root = ElementTree.parse(source=self.coverage_report_path).getroot()
+        coverage_score = root.attrib["line-rate"]
+        if coverage_score == "1":
+            return "100%"
+
+        return f"{round(100.0 * float(coverage_score), 1)}%"
+
 
 def main() -> None:
     """Main function for badge generation."""
@@ -99,10 +124,20 @@ def main() -> None:
         default="reports/unit-tests.xml",
         help="Path to the unit tests XML report",
     )
+    parser.add_argument(
+        "-crp",
+        "--coverage-report-path",
+        type=str,
+        default="reports/coverage.xml",
+        help="Path to the unit test coverage XML report",
+    )
     args = parser.parse_args()
 
     badge_generator = BadgeGenerator(
-        output_dir=args.output_dir, python_version=args.python_version, tests_report_path=args.tests_report_path
+        output_dir=args.output_dir,
+        python_version=args.python_version,
+        tests_report_path=args.tests_report_path,
+        coverage_report_path=args.coverage_report_path,
     )
     badge_generator.generate_badges()
 
