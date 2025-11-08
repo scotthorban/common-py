fac8c66544bdbadf9b877918745b50809cb48dc3
scotthorban <scotthorban@gmail.com>
2025-11-08 23:57:17 +0000

    test: #5 refactor generate_badges tests add get_coverage_results() tests

--- tests/unit/utils/test_generate_badges.py
+++ tests/unit/utils/test_generate_badges.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from unittest.mock import MagicMock, patch
 
+import pytest
 from anybadge import colors
 from defusedxml.ElementTree import fromstring
 
@@ -31,9 +32,17 @@ def generate_unittest_element_tree(
     )
 
 
+def generate_coverage_report_tree(line_rate: float) -> ET.ElementTree:
+    """Helper function to generate an ElementTree for coverage report XML."""
+    return ET.ElementTree(element=fromstring(text=f"""<coverage line-rate="{line_rate}"></coverage>"""))
+
+
 UNITTEST_XML_ALL_PASS = generate_unittest_element_tree(num_errors=0, num_failures=0, num_skipped=0, num_tests=1)
 UNITTEST_XML_PASS_AND_SKIP = generate_unittest_element_tree(num_errors=0, num_failures=0, num_skipped=1, num_tests=2)
 UNITTEST_XML_FAIL = generate_unittest_element_tree(num_errors=0, num_failures=1, num_skipped=1, num_tests=3)
+
+COVERAGE_XML_100_pct = generate_coverage_report_tree(line_rate=1)
+COVERAGE_XML_UNROUNDED_pct = generate_coverage_report_tree(line_rate=0.1234)
 
 
 class TestBadgeGenerator(unittest.TestCase):
@@ -77,6 +86,11 @@ class TestBadgeGenerator(unittest.TestCase):
         main()
         mock_generate_badges.assert_called_once()
 
+    def test_get_unittest_results_raises_on_missing_tests_report_path(self) -> None:
+        badge_generator = self.badge_generator
+        badge_generator.tests_report_path = None
+        pytest.raises(AttributeError, badge_generator.get_unittest_results)
+
     @patch(target="common_py.utils.generate_badges.parse", return_value=UNITTEST_XML_ALL_PASS)
     def test_get_unittest_results_all_passed(self, _mock_parse: MagicMock) -> None:
         assert self.badge_generator.get_unittest_results() == ("1 passed", colors.Color.GREEN)
@@ -88,3 +102,16 @@ class TestBadgeGenerator(unittest.TestCase):
     @patch(target="common_py.utils.generate_badges.parse", return_value=UNITTEST_XML_FAIL)
     def test_get_unittest_results_fail(self, _mock_parse: MagicMock) -> None:
         assert self.badge_generator.get_unittest_results() == ("1 failed 1 passed", colors.Color.RED)
+
+    def test_get_coverage_results_raises_on_missing_coverage_report_path(self) -> None:
+        badge_generator = self.badge_generator
+        badge_generator.coverage_report_path = None
+        pytest.raises(AttributeError, badge_generator.get_coverage_results)
+
+    @patch(target="common_py.utils.generate_badges.parse", return_value=COVERAGE_XML_100_pct)
+    def test_get_coverage_results_100_pct(self, _mock_parse: MagicMock) -> None:
+        assert self.badge_generator.get_coverage_results() == 100
+
+    @patch(target="common_py.utils.generate_badges.parse", return_value=COVERAGE_XML_UNROUNDED_pct)
+    def test_get_coverage_results_50_pct(self, _mock_parse: MagicMock) -> None:
+        assert self.badge_generator.get_coverage_results() == 12.3
