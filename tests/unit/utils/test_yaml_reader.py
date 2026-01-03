374ab7d1c3ce9f0ee075fa27fd866e16e3b28768
scotthorban <scotthorban@gmail.com>
2026-01-03 11:50:02 +0000

    style: #14 rename yaml join constructor test

--- tests/unit/utils/test_yaml_reader.py
+++ tests/unit/utils/test_yaml_reader.py
@@ -5,7 +5,7 @@ TEST_FILE_PATH = TESTS_ROOT_DIR.joinpath("unit/utils/test_yaml_file.yaml")
 
 
 class TestYamlReader:
-    def test_read_yaml(self) -> None:
+    def test_join_constructor(self) -> None:
         data = read_yaml(path=TEST_FILE_PATH)
         assert data["test_join_constructor"] == {
             "key1": "value1",
